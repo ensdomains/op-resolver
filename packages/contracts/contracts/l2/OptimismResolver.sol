d3d2cc072eb8c6d0ba39d1d87c92153d3b4b1aaa
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-11-16 19:22:22 +0000

    WIP add cointype

--- packages/contracts/contracts/l2/OptimismResolver.sol
+++ packages/contracts/contracts/l2/OptimismResolver.sol
@@ -1,22 +1,50 @@
 pragma solidity ^0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+uint256 constant COIN_TYPE_ETH = 60;
 
 contract OptimismResolver is Ownable {
     mapping(bytes32 => address) addresses;
+    mapping(bytes32 => mapping(uint256 => bytes)) _addresses;
 
     event AddrChanged(bytes32 indexed node, address a);
 
-    function setAddr(bytes32 node, address addr) public onlyOwner {
-        addresses[node] = addr;
-        emit AddrChanged(node, addr);
+    function setAddr(
+        bytes32 node,
+        uint256 coinType,
+        bytes memory a
+    ) public onlyOwner {
+        _addresses[node][coinType] = a;
+        emit AddrChanged(node, bytesToAddress(a));
     }
 
-    function addr(bytes32 node) public view returns (address) {
-        return addresses[node];
+    function addr(bytes32 node) public view returns (bytes memory) {
+        return _addresses[node][COIN_TYPE_ETH];
     }
 
-    function addr(bytes32 node, uint256) public view returns (address) {
-        return addresses[node];
+    function addr(bytes32 node, uint256 coinType)
+        public
+        view
+        returns (bytes memory)
+    {
+        return _addresses[node][coinType];
+    }
+
+    function bytesToAddress(bytes memory b)
+        internal
+        pure
+        returns (address payable a)
+    {
+        require(b.length == 20);
+        assembly {
+            a := div(mload(add(b, 32)), exp(256, 12))
+        }
+    }
+
+    function addressToBytes(address a) internal pure returns (bytes memory b) {
+        b = new bytes(20);
+        assembly {
+            mstore(add(b, 32), mul(a, exp(256, 12)))
+        }
     }
 }
--- packages/contracts/test/l2/resolver-test.js
+++ packages/contracts/test/l2/resolver-test.js
@@ -1,9 +1,11 @@
 const { expect } = require("chai");
-
+const hre = require("hardhat")
+const ethers = hre.ethers
 const NODE = "0xeb4f647bea6caa36333c816d7b46fdcb05f9466ecacc140ea8c66faf15b3d9f1"; // namehash('test.eth')
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("OptimismResolver", function() {
-  it("Should return an address once set", async function() {
+  it.only("Should return an address once set", async function() {
     const accounts = await ethers.getSigners();
     const address = await accounts[0].getAddress();
 
@@ -13,5 +15,12 @@ describe("OptimismResolver", function() {
 
     await resolver.setAddr(NODE, address);
     expect(await resolver['addr(bytes32)'](NODE)).to.equal(address);
+    const provider = hre.network.provider
+    console.log(helpers.getStorageAt)
+    const addrSlot = ethers.utils.keccak256(NODE + '00'.repeat(31) + '01');
+    addressData = await helpers.getStorageAt(resolver.address, addrSlot)
+    console.log({
+      addressData
+    })
   });
 });
