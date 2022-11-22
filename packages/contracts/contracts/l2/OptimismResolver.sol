c64aff38e5d763bc59a96c777a24b58e2d0b742e
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-11-22 09:22:48 +0000

    Add multi coin type

--- packages/contracts/contracts/l2/OptimismResolver.sol
+++ packages/contracts/contracts/l2/OptimismResolver.sol
@@ -4,33 +4,32 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 uint256 constant COIN_TYPE_ETH = 60;
 
 contract OptimismResolver is Ownable {
-    mapping(bytes32 => address) addresses;
-    mapping(bytes32 => mapping(uint256 => bytes)) _addresses;
+    mapping(bytes32 => mapping(uint256 => bytes32)) _addresses; // 1
+    mapping(bytes => bytes32) _bytes; // 2
 
     event AddrChanged(bytes32 indexed node, address a);
 
     function setAddr(
         bytes32 node,
         uint256 coinType,
-        bytes memory a
+        bytes32 a
     ) public onlyOwner {
         _addresses[node][coinType] = a;
-        emit AddrChanged(node, bytesToAddress(a));
     }
 
-    function addr(bytes32 node) public view returns (bytes memory) {
+    function addr(bytes32 node) public view returns (bytes32) {
         return _addresses[node][COIN_TYPE_ETH];
     }
 
     function addr(bytes32 node, uint256 coinType)
         public
         view
-        returns (bytes memory)
+        returns (bytes32)
     {
         return _addresses[node][coinType];
     }
 
-    function bytesToAddress(bytes memory b)
+    function bytesToAddress(bytes32 b)
         internal
         pure
         returns (address payable a)
@@ -41,10 +40,16 @@ contract OptimismResolver is Ownable {
         }
     }
 
-    function addressToBytes(address a) internal pure returns (bytes memory b) {
-        b = new bytes(20);
-        assembly {
-            mstore(add(b, 32), mul(a, exp(256, 12)))
-        }
+    function mapIntLocation(uint256 slot, uint256 key) public pure returns (uint256) {
+        return uint256(keccak256(
+          abi.encode(key, slot)
+        ));
     }
+
+    function mapLocation(bytes32 slot, uint256 key) public pure returns (uint256) {
+        return uint256(keccak256(
+          abi.encode(key, slot)
+        ));
+    }
+
 }
--- packages/contracts/test/l2/resolver-test.js
+++ packages/contracts/test/l2/resolver-test.js
@@ -2,7 +2,14 @@ const { expect } = require("chai");
 const hre = require("hardhat")
 const ethers = hre.ethers
 const NODE = "0xeb4f647bea6caa36333c816d7b46fdcb05f9466ecacc140ea8c66faf15b3d9f1"; // namehash('test.eth')
+const COIN_TYPE_ETH = 60;
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const { BigNumber, utils } = ethers
+const { hexZeroPad } = utils
+
+function bytes32ify(value) {
+  return hexZeroPad(BigNumber.from(value).toHexString(), 32);
+}
 
 describe("OptimismResolver", function() {
   it.only("Should return an address once set", async function() {
@@ -12,15 +19,19 @@ describe("OptimismResolver", function() {
     const Resolver = await ethers.getContractFactory("OptimismResolver");
     const resolver = await Resolver.deploy();
     await resolver.deployed();
+    console.log(1, address)
+    const addrBytes = bytes32ify(address)
+    // const addrBytes = ethers.utils.formatBytes32String('hello')
 
-    await resolver.setAddr(NODE, address);
-    expect(await resolver['addr(bytes32)'](NODE)).to.equal(address);
-    const provider = hre.network.provider
-    console.log(helpers.getStorageAt)
-    const addrSlot = ethers.utils.keccak256(NODE + '00'.repeat(31) + '01');
-    addressData = await helpers.getStorageAt(resolver.address, addrSlot)
     console.log({
-      addressData
+      address,
+      addrBytes
     })
+    await resolver.setAddr(NODE, COIN_TYPE_ETH, addrBytes);
+    console.log(await resolver['addr(bytes32)'](NODE))
+    const nodeLoc = await resolver.mapIntLocation(1, NODE);
+    const coinLoc = await resolver.mapLocation(nodeLoc, COIN_TYPE_ETH);
+    const addr2Value = await helpers.getStorageAt(resolver.address, coinLoc);
+    console.log({nodeLoc, coinLoc, addr2Value})
   });
 });
